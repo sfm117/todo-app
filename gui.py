4708e933d99af581ca5a852e21c2683ad5e07ec0
Steve <stevemari77@gmail.com>
2023-02-06 14:59:35 -0500

    Adjusted sizes of add and complete buttons

--- gui.py
+++ gui.py
@@ -2,16 +2,18 @@ import functions
 import PySimpleGUI as sg
 import time
 
-sg.theme("DarkGreen5")
+sg.theme("Black")
 
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add", size=10)
+add_button = sg.Button(image_size=(50, 50), image_source="add.png", mouseover_colors="LightBlue2",
+                       tooltip="Add Todo", key="Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(image_size=(50, 50), image_source="complete.png", mouseover_colors="LightBlue2",
+                            tooltip="Complete Todo", key="Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
