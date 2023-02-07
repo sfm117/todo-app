4eafe212cfe6990fb340050beb2adf277c2791cf
Steve <stevemari77@gmail.com>
2023-02-07 12:01:54 -0500

    Reverted add and complete buttons to simple form

--- gui.py
+++ gui.py
@@ -7,13 +7,11 @@ sg.theme("Black")
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button(image_size=(50, 50), image_source="add.png", mouseover_colors="LightBlue2",
-                       tooltip="Add Todo", key="Add")
+add_button = sg.Button("Add", size=10)
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
-complete_button = sg.Button(image_size=(50, 50), image_source="complete.png", mouseover_colors="LightBlue2",
-                            tooltip="Complete Todo", key="Complete")
+complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
