08f56eeda11322c6fe5cd1d379cb6a72b7f1703f
Steve <stevemari77@gmail.com>
2023-04-28 11:58:55 -0400

    changed various text

--- web.py
+++ web.py
@@ -14,8 +14,8 @@ def add_todo():
 
 
 st.title("Daily Task Tracker")
-st.subheader("This is my todo app.")
-st.write("This app is designed to increase your productivity.")
+st.subheader("This is a work in progress.")
+st.write("This app is designed to increase productivity.")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
